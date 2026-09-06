eb7708ca27a40ed6a5924b122753192765079f0d
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-06 23:15:19 +0800

    fix: Zed to use AMDGPU instead of NVIDIA GPU

--- home/ongyean/modules/env.nix
+++ home/ongyean/modules/env.nix
@@ -3,6 +3,6 @@
 {
   xdg.configFile."uwsm/env".source = "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
   home.sessionVariables = {
-
+    ZED_DEVICE_ID = "1681";
   };
 }
