4c8eed33e35ec6dea9730344913c5853b7bfa3cf
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-13 00:51:29 +0800

    ongyean-laptop: Block built-in webcam and add WireGuard interface

--- hosts/ongyean-laptop/default.nix
+++ hosts/ongyean-laptop/default.nix
@@ -22,8 +22,32 @@
 
   networking.hostName = "ongyean-laptop"; # Define your hostname.
 
+  # WireGuard VPN for ongyean-laptop
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      autostart = false;
+      address = [ "192.168.2.2/24" ];
+      dns = [ "1.1.1.1" "1.0.0.1" ];
+      mtu = 1280;
+      privateKeyFile = "/var/lib/wireguard/wg0.key";
+
+      peers = [{
+        publicKey = "lyVgaZdfi/M5LR0/cyZhSZlx1URX4ussEBv1Vj94rHA=";
+        allowedIPs = [ "0.0.0.0/0" "::/0" ];
+        endpoint = "161.118.252.195:51820";
+        persistentKeepalive = 25;
+      }];
+    };
+  };
+
   # Absolutely proprietary
   nixpkgs.config.allowUnfree = true;
+
+  # Built-in webcam is broken on this laptop :(
+  # Hence it is blocked via udev rule to prevent being waken up
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="30c9", ATTR{idProduct}=="0065", ATTR{authorized}="0"
+  '';
 
   # Unlock the LUKS2 container first so that the partitions can mount
   boot.initrd.luks.devices."nixos" = {
