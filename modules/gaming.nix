42af8dcbc0b45094a11a53a00def296233a19f7b
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-12 19:29:30 +0800

    Add icon to GameMode notification

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -18,8 +18,8 @@
         renice = 10;
       };
        custom = {
-        start = "${pkgs.libnotify}/bin/notify-send -i game -a 'FeralInteractive GameMode' 'Game Mode is ON'";
-        end = "${pkgs.libnotify}/bin/notify-send -i game -a 'FeralInteractive GameMode' 'Game Mode is OFF'";
+        start = "${pkgs.libnotify}/bin/notify-send -i game -a 'FeralInteractive GameMode' -i applications-games 'Game Mode is ON'";
+        end = "${pkgs.libnotify}/bin/notify-send -i game -a 'FeralInteractive GameMode' -i applications-games 'Game Mode is OFF'";
       };
     };
   };
