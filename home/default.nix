ba41fd2ac6b365ef1106d84240804e2f5016cf46
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-13 00:54:54 +0800

    build: Add all-users fix for launching terminal programs from Dolphin

--- home/default.nix
+++ home/default.nix
@@ -10,5 +10,13 @@
     useUserPackages = true;
     extraSpecialArgs = { inherit inputs; };
     startAsUserService = true;
+
+    sharedModules = [{
+      # Global fix for Dolphin being unable to open Neovim
+      qt.kde.settings = {
+        kdeglobals.General.TerminalApplication = "alacritty";
+        kdeglobals.General.TerminalService = "Alacritty.desktop";
+      };
+    }];
   };
 }
