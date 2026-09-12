128924926db826b048ce1afc95a289e16f5863af
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-13 01:12:21 +0800

    build: Networking optimizations

--- modules/common/networking.nix
+++ modules/common/networking.nix
@@ -1,17 +1,31 @@
 { ... }:
 
 {
-  # TODO: Set up good DNS provider and change to systemd-resolved
-  networking.networkmanager = {
-    enable = true;
-    dns = "default";
-    ethernet.macAddress = "random";
-    wifi.macAddress = "random";
-    wifi.backend = "wpa_supplicant";
-    wifi.powersave = true;
+  networking = {
+    networkmanager = {
+      enable = true;
+      dns = "systemd-resolved";
+      ethernet.macAddress = "random";
+      wifi.macAddress = "random";
+      wifi.backend = "iwd";
+      wifi.powersave = true;
+    };
+
+    # Use Cloudflare's public DNS resolver
+    nameservers = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+      "2606:4700:4700::1111#one.one.one.one"
+      "2606:4700:4700::1001#one.one.one.one"
+    ];
+
+    firewall = {
+      enable = true;
+    };
   };
 
-  networking.firewall = {
+  services.resolved = {
     enable = true;
+    settings.Resolve.DNSOverTLS = true;
   };
 }
