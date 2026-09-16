1a5a91d42b21d2f444a65f731d5c2b030abe9449
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-16 23:05:43 +0800

    music-production: Add some plugins and to-do list

--- home/ongyean/modules/music-production.nix
+++ home/ongyean/modules/music-production.nix
@@ -6,5 +6,17 @@
     qjackctl
     guitarix
     audacity
+
+    # Audio plugins
+    lsp-plugins
+    zam-plugins
   ];
+
+  # TODO: Add the following plugins
+  # jdrummer.vst3
+  # DuskVerb.clap
+  # ZynAddSubFX.vst
+  # Surge XT
+  # sforzando
+  # MDA VST
 }
