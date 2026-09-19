633efbaf109613a3c49aee3a3bd74cd5ed6e7518
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-19 11:11:41 +0800

    ops: Remove linux-firmware pin as the DMCUB bug was resolved in version 20260916

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pinnedPkgs.url = "github:NixOS/nixpkgs/8ce4ef6cb6f871616146b9fe26d2a5ae594e94fe";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -11,16 +10,11 @@
     lanzaboote.url = "github:nix-community/lanzaboote/v1.1.0";
     lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, pinnedPkgs, home-manager, chaotic, lanzaboote, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, chaotic, lanzaboote, ... }: {
     nixosConfigurations."ongyean-laptop" = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/ongyean-laptop
-        {
-          nixpkgs.overlays = [(final: prev: {
-            linux-firmware = pinnedPkgs.legacyPackages.x86_64-linux.linux-firmware;
-          })];
-        }
       ];
     };
   };
