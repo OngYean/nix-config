e2e1b81aa7571c1656cb150a433b938cf51b22d8
OngYean <79567278+OngYean@users.noreply.github.com>
2026-09-25 21:15:02 +0800

    Update ongyean-pc config

--- hosts/ongyean-pc/default.nix
+++ hosts/ongyean-pc/default.nix
@@ -7,14 +7,16 @@
   imports = [
     ../../modules/common
     ../../modules/apps.nix
-    ../../modules/fonts.nix
-    ../../modules/gaming.nix
+    # TODO: Enable gaming when ready
+    # ../../modules/gaming.nix
+    ../../modules/hyprland.nix
+    ../../modules/phone-connect.nix
     # TODO: Enable secure boot when ready
     # ../../modules/secure-boot.nix
     ../../modules/services.nix
     ../../modules/tweaks.nix
     ../../modules/hardware/nvidia.nix
-    ../../home/ongyean
+    ../../users/ongyean
     ./hardware-configuration.nix
     inputs.chaotic.nixosModules.default
   ];
@@ -24,9 +26,11 @@
   # Absolutely proprietary
   nixpkgs.config.allowUnfree = true;
 
+  services.displayManager.noctalia-greeter.enable = true;
+
   # No need to save power on a desktop's Wi-Fi
   networking.networkmanager.wifi.powersave = lib.mkForce false;
-  
+
   # Unlock the LUKS2 container first so that the partitions can mount
   boot.initrd.luks.devices."nixos" = {
     device = "/dev/disk/by-label/nixos";
@@ -35,4 +39,6 @@
 
   # Force the boot partition to be found via its label
   fileSystems."/boot".device = lib.mkForce "/dev/disk/by-label/boot";
+
+  services.lact.enable = true;
 }
